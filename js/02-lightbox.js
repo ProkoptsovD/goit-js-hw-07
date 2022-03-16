281bf80fdf4954006f5f5066d0fbc401be050c66
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-16 14:06:32 +0200

    02-task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryCardsMarkup = makeGalleryCardsMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryCardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+	overlayOpacity: 0.8,
+	fadeSpeed: 200,
+	maxZoom: 2,
+	scrollZoomFactor: 0.2,
+	captionsData: 'alt',
+	captionDelay: 250,
+});
+
+function makeGalleryCardsMarkup(images) {
+	return images.map(({ description, original, preview }) => {
+		return `
+                <li>
+                    <a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>
+                </li>
+                    `;
+	}).join``;
+}
